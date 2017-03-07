bf32779ecabeb178b11a9fdccbb5dafc1cf6cb50
Jérémy LEGROS <legros.j@sfeir.com>
2017-03-07 17:49:51 +0100

    script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,20 +3,28 @@
 set -o errexit # Exit on error
 
 ng build --prod -aot
-git add -A dist
-git commit -am "pre-deploy"
+#git add dist
+#git stash
+#git commit -am "pre-deploy"
 # on passe sur le master et on recupère le rep build
 git checkout master
 # on nettoie la remote
-rm -rf dist
-git push -f origin master
+git status
+ls -al
+rm *.js index.html favicon.icon *.css
+cp dist/* .
+rm -r dist
+git add -u
+git status
+#git push -f origin master
 # On recupère les dernieres source buildes
-git checkout dev-ng -- dist
-git add -A dist
-git commit -am "deploy 1"
+echo "> clean up done."
+#git checkout dev-ng -- dist
+#git add -A dist
+#git commit -am "deploy 1"
 #git push -f origin master
 #git commit -am "auto-deploy 2"
 #git subtree push --prefix dist origin master
-git push origin `git subtree split --prefix dist master`:master --force
+#git push origin `git subtree split --prefix dist master`:master --force
 
 git checkout dev-ng
