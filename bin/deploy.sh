f8889a58cec74f64a1499f69b17f89fbce40f3af
Jérémy LEGROS <legros.j@sfeir.com>
2017-03-07 17:36:41 +0100

    pre-deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,11 +7,13 @@ git add -A dist
 git commit -am "pre-deploy"
 # on passe sur le master et on recupère le rep build
 git checkout master
+# on nettoie la remote
 git rm -rf dist
 git push -f origin master
+# On recupère les dernieres source buildes
 git checkout dev-ng -- dist
-git add -A dist
-git commit -am "deploy"
+#git add -A dist
+git commit -am "deploy 1"
 #git push -f origin master
 #git commit -am "auto-deploy 2"
 #git subtree push --prefix dist origin master
